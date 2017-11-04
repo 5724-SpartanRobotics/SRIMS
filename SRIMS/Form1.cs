e41d7e778418afdd94ac6e5966292963f1c95d18
FishDog5000 <the.programmer.ninja.3.14@gmail.com>
2017-11-04 14:46:42 -0400

    Fixed it crashing the computer with a million settings windows.

--- SRIMS/Form1.cs
+++ SRIMS/Form1.cs
@@ -11,175 +11,175 @@ using System.IO;
 
 namespace SRIMS
 {
-    public partial class Form1 : Form
-    {
+	public partial class Form1 : Form
+	{
 
-        string dbloc = Properties.Settings.Default.dbloc;
+		string dbloc = Properties.Settings.Default.dbloc;
 
-        public Form1()
-        {
-            InitializeComponent();
-        }
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        //Real Code Begins Here:
+		//Real Code Begins Here:
 
-        item x = new item();
+		item x = new item();
 
-        public List <item> inv = new List<item>();
+		public List<item> inv = new List<item>();
 
-        private void init()
-        {
-            dbloc = Properties.Settings.Default.dbloc;
-            while (dbloc == null || dbloc == "")
-            {
-                Settings settings = new Settings();
-                settings.Show();
-            }
-            try
-            {
-                StreamReader sr = new StreamReader(dbloc);
-                string head = sr.ReadLine();
+		private void init()
+		{
+			dbloc = Properties.Settings.Default.dbloc;
+			if (dbloc == null || dbloc == "")
+			{
+				Settings settings = new Settings();
+				settings.Show();
+			}
+			try
+			{
+				StreamReader sr = new StreamReader(dbloc);
+				string head = sr.ReadLine();
 
-                //Console.WriteLine(head);
+				//Console.WriteLine(head);
 
-                string headId = head.Split(',')[0];
-                //Console.WriteLine(headId);
-                headId = headId.Remove(0, 2);
-                //Console.WriteLine(headId);
-                int itemcount = Int32.Parse(headId);
-                //Console.WriteLine(itemcount);
+				string headId = head.Split(',')[0];
+				//Console.WriteLine(headId);
+				headId = headId.Remove(0, 2);
+				//Console.WriteLine(headId);
+				int itemcount = Int32.Parse(headId);
+				//Console.WriteLine(itemcount);
 
-                for (int i = 0; i < itemcount; i++)
-                {
-                    string currentitem = sr.ReadLine();
+				for (int i = 0; i < itemcount; i++)
+				{
+					string currentitem = sr.ReadLine();
 
-                    string[] parts = currentitem.Split(',');
+					string[] parts = currentitem.Split(',');
 
-                    //Console.WriteLine(parts[0] + "," + parts[1] + "," + parts[2] + "," + parts[3] + "," + parts[4] + "," + parts[5]);
+					//Console.WriteLine(parts[0] + "," + parts[1] + "," + parts[2] + "," + parts[3] + "," + parts[4] + "," + parts[5]);
 
-                    inv.Add(new item((Int32.Parse(parts[0])),parts[1],parts[2],parts[3],parts[4],Int32.Parse(parts[5])));
+					inv.Add(new item((Int32.Parse(parts[0])), parts[1], parts[2], parts[3], parts[4], Int32.Parse(parts[5])));
 
-                }
+				}
 
-                foreach (item x in inv)
-                {
-                   // Console.WriteLine(x);
-                }
-
-
-                sr.Close();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                Settings settings = new Settings();
-                settings.Show();
-                settings.FormClosed += Settings_FormClosed;
-                
-            }
-
-            //StreamReader sr = new StreamReader(dbloc);
-
-        }
-
-        private void Settings_FormClosed(object sender, FormClosedEventArgs e)
-        {
-            init();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            init();
-            dehighlightSelectors();
-        }
-
-        private void dehighlightSelectors()
-        {
-            CheckOutSelected.Visible = false;
-            CheckInSelected.Visible = false;
-            ViewDatabaseSelected.Visible = false;
-            AddItemSelected.Visible = false;
-            SearchSelected.Visible = false;
-            CheckOutLogSelected.Visible = false;
-
-            checkout1.Visible = false;
-            checkin1.Visible = false;
-            viewDB1.Visible = false;
-            addItem1.Visible = false;
-            search1.Visible = false;
-            checkoutLog1.Visible = false;
+				foreach (item x in inv)
+				{
+					// Console.WriteLine(x);
+				}
 
 
-        }
+				sr.Close();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				Settings settings = new Settings();
+				settings.Show();
+				settings.FormClosed += Settings_FormClosed;
 
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-            
-        }
-        // CheckOut
-        private void button1_Click(object sender, EventArgs e)
-        {
-            dehighlightSelectors();
-            CheckOutSelected.Visible = true;
-            checkout1.Visible = true;
+			}
 
-        }
-        // CheckIn
-        private void button2_Click(object sender, EventArgs e)
-        {
-            dehighlightSelectors();
-            CheckInSelected.Visible = true;
-            checkin1.Visible = true;
+			//StreamReader sr = new StreamReader(dbloc);
 
-        }
-        // ViewDB
-        private void button3_Click(object sender, EventArgs e)
-        {
-            dehighlightSelectors();
-            ViewDatabaseSelected.Visible = true;
-            viewDB1.Visible = true;
+		}
 
-            viewDB1.ext(inv);
+		private void Settings_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			init();
+		}
 
-        }
-        // Search
-        private void button4_Click(object sender, EventArgs e)
-        {
-            dehighlightSelectors();
-            SearchSelected.Visible = true;
-            search1.Visible = true;
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			init();
+			dehighlightSelectors();
+		}
 
-            search1.popdb(inv);
-            search1.reset();
+		private void dehighlightSelectors()
+		{
+			CheckOutSelected.Visible = false;
+			CheckInSelected.Visible = false;
+			ViewDatabaseSelected.Visible = false;
+			AddItemSelected.Visible = false;
+			SearchSelected.Visible = false;
+			CheckOutLogSelected.Visible = false;
 
-        }
-        // AddItem
-        private void button5_Click(object sender, EventArgs e)
-        {
-            dehighlightSelectors();
-            AddItemSelected.Visible = true;
-            addItem1.Visible = true;
-
-            addItem1.reload(true);
-
-            addItem1.setDB(inv);
+			checkout1.Visible = false;
+			checkin1.Visible = false;
+			viewDB1.Visible = false;
+			addItem1.Visible = false;
+			search1.Visible = false;
+			checkoutLog1.Visible = false;
 
 
+		}
 
-        }
-        // CheckOutLog
-        private void button6_Click(object sender, EventArgs e)
-        {
-            dehighlightSelectors();
-            CheckOutLogSelected.Visible = true;
-            checkoutLog1.Visible = true;
+		private void panel2_Paint(object sender, PaintEventArgs e)
+		{
 
-        }
-        // Settings
-        private void button7_Click(object sender, EventArgs e)
-        {
-            Settings settings = new Settings();
-            settings.Show();
-        }
-    }
+		}
+		// CheckOut
+		private void button1_Click(object sender, EventArgs e)
+		{
+			dehighlightSelectors();
+			CheckOutSelected.Visible = true;
+			checkout1.Visible = true;
+
+		}
+		// CheckIn
+		private void button2_Click(object sender, EventArgs e)
+		{
+			dehighlightSelectors();
+			CheckInSelected.Visible = true;
+			checkin1.Visible = true;
+
+		}
+		// ViewDB
+		private void button3_Click(object sender, EventArgs e)
+		{
+			dehighlightSelectors();
+			ViewDatabaseSelected.Visible = true;
+			viewDB1.Visible = true;
+
+			viewDB1.ext(inv);
+
+		}
+		// Search
+		private void button4_Click(object sender, EventArgs e)
+		{
+			dehighlightSelectors();
+			SearchSelected.Visible = true;
+			search1.Visible = true;
+
+			search1.popdb(inv);
+			search1.reset();
+
+		}
+		// AddItem
+		private void button5_Click(object sender, EventArgs e)
+		{
+			dehighlightSelectors();
+			AddItemSelected.Visible = true;
+			addItem1.Visible = true;
+
+			addItem1.reload(true);
+
+			addItem1.setDB(inv);
+
+
+
+		}
+		// CheckOutLog
+		private void button6_Click(object sender, EventArgs e)
+		{
+			dehighlightSelectors();
+			CheckOutLogSelected.Visible = true;
+			checkoutLog1.Visible = true;
+
+		}
+		// Settings
+		private void button7_Click(object sender, EventArgs e)
+		{
+			Settings settings = new Settings();
+			settings.Show();
+		}
+	}
 }
--- SRIMS/item.cs
+++ SRIMS/item.cs
@@ -6,52 +6,52 @@ using System.Threading.Tasks;
 
 namespace SRIMS
 {
-    public class item
-    {
-        int id = 0;
-        string loc = "Location";
-        string cat = "Category";
-        string name = "Item";
-        string desc = "Item Description";
-        int qt = 0;
+	public class item
+	{
+		int id = 0;
+		string loc = "Location";
+		string cat = "Category";
+		string name = "Item";
+		string desc = "Item Description";
+		int qt = 0;
 
-        public item()
-        {
-         
-        }
+		public item()
+		{
 
-        public item(int id, string loc, string cat, string name, string desc, int qt)
-        {
-            this.id = id;
-            this.loc = loc;
-            this.cat = cat;
-            this.name = name;
-            this.desc = desc;
-            this.qt = qt;
+		}
 
-        }
+		public item(int id, string loc, string cat, string name, string desc, int qt)
+		{
+			this.id = id;
+			this.loc = loc;
+			this.cat = cat;
+			this.name = name;
+			this.desc = desc;
+			this.qt = qt;
 
-        public override string ToString()
-        {
-            //return null;
-            return this.id + "," + this.loc + "," + this.cat + "," + this.name + "," + this.desc + "," + this.qt;
-        }
-        public int getid()
-        {
-            return id;
-        }
-        public string getName()
-        {
-            return name;
-        }
-        public string getLoc()
-        {
-            return loc;
-        }
-        public string getCat()
-        {
-            return cat;
-        }
-    }
+		}
+
+		public override string ToString()
+		{
+			//return null;
+			return this.id + "," + this.loc + "," + this.cat + "," + this.name + "," + this.desc + "," + this.qt;
+		}
+		public int getid()
+		{
+			return id;
+		}
+		public string getName()
+		{
+			return name;
+		}
+		public string getLoc()
+		{
+			return loc;
+		}
+		public string getCat()
+		{
+			return cat;
+		}
+	}
 
 }
